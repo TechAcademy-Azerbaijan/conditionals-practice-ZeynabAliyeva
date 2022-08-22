e07c45932e85233da2acc17ac9e0d3329660fb41
ZeynabAliyeva <103027531+ZeynabAliyeva@users.noreply.github.com>
2022-08-22 20:10:01 +0400

    task1clean

--- task-1/main.js
+++ task-1/main.js
@@ -5,10 +5,28 @@ prompt.start();
 prompt.get('input', function (err, result) {
   
    let n = parseInt(result.input);
-   let a = n**3 + 5*n;
-   let b = n**2 -2*n + 4;
-   if(a%b !== 0){
-   console.log(b);
+   let a;
+   if(-10000 <= n && n <=10000){
+      if(n>=10){
+      a = n**3 + 5*n 
+      }else{
+      a = n**2 -2*n + 4;
+      }
+      console.log(a);
    }
+
+
+
+
+
+
+
+
+
+   //let a = n**3 + 5*n;
+   // let b = n**2 -2*n + 4;
+   // if(a%b !== 0){
+   // console.log(b);
+   // }
   
 });
