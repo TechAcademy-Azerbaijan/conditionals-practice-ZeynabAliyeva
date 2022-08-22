8f31c9047da0add512532f366ad025a4d05f068e
ZeynabAliyeva <103027531+ZeynabAliyeva@users.noreply.github.com>
2022-08-22 20:11:58 +0400

    task1clean

--- task-2/main.js
+++ task-2/main.js
@@ -6,13 +6,13 @@ prompt.get('input', function (err, result) {
   
     let input = result.input;
     let n = input.split(',');
-    let x = parseInt(n[0]);
-    let a = parseInt(n[1]);
-    let b = parseInt(n[2]);
-    if(x < a || x > b){
-        console.log("OUT");
-    }else if(x > a || x < b){
-        console.log("IN");
+    let x = parseInt(n[0]); //7
+    let a = parseInt(n[1]); //2
+    let b = parseInt(n[2]); //7
+    if(x > a && x <= b){
+        console.log("IN")
+    }else{
+        console.log("OUT")
     }
     
 });
